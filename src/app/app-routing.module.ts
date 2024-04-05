e2c973d0a35431ed4dcf66bf365b0d06e655f4c1
orajiakuchibuchi <somtobuchi@gmail.com>
2024-04-05 15:56:56 +0100

    updated kitchen AI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SockResolver } from './shared/resolvers/socket/sock.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    loadChildren: ()=> import('./home/home.module').then(m => m.HomeModule),
+    resolve:{
+      fromSock: SockResolver
+    }
   }
 ];
 
---
+++ src/app/shared/components/Index.ts
@@ -0,0 +1,7 @@
+import { CalendarComponent } from "./calendar/calendar.component";
+import { PageLoaderComponent } from "./page-loader/page-loader.component";
+
+export {
+    PageLoaderComponent,
+    CalendarComponent
+}

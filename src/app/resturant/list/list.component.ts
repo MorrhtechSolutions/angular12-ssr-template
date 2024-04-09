466983cac7eb25df8a250b21f4251e10e1e3ea44
chinemerem2525 <chubexchange@gmail.com>
2024-04-09 02:26:35 -0700

    Resturant

--- src/app/resturant/list/list.component.ts
+++ src/app/resturant/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceService } from 'src/app/shared/services/client/device.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DeviceService) { }
 
   ngOnInit(): void {
   }
+
+  showAddToCart(){
+    this.ds.oSuccessNotification('Added to cart', 'You have successfully added this item to cart')
+  }
+
 
 }

6ba261e2aa8ec6d32358d978cb37836ec09ea1db
chinemerem2525 <chubexchange@gmail.com>
2024-04-10 16:16:07 -0700

    stop-for-11/04/2024

--- src/app/order/search/search.component.ts
+++ src/app/order/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { DeviceService } from 'src/app/shared/services/client/device.service';
+import { ScriptsService } from 'src/app/shared/services/client/scripts.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private ds: DeviceService, private scriptService: ScriptsService) { }
 
   ngOnInit(): void {
   }
+  search() {
+    const searchTerm = (document.querySelector('[name="searchTerm"]') as HTMLInputElement).value;
+    this.router.navigate(['/order/view/'+'05'], { queryParams: { term: searchTerm } });
+  }
 
 }
+
+
+
